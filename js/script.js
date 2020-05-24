7e6a3a91eaca27fd6f5af23b8d02f16ebd56faae
AndreiManya <1234and95@gmail.com>
2020-05-24 16:15:44 +0300

    responiseveFlixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 window.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
-  let nav = document.querySelector('.nav'),
-      scrolled = document.querySelectorAll('.scro'),
-      btns = document.querySelectorAll('.bt'),
-      doc = document.getElementsByClassName('contentThree')[0];
+  // let nav = document.querySelector('.nav'),
+  //     scrolled = document.querySelectorAll('.scro'),
+  //     btns = document.querySelectorAll('.bt'),
+  //     doc = document.getElementsByClassName('contentThree')[0];
   
-  nav.addEventListener('click', function(event) {
-    if(event.target && event.target.classList.contains('bt')){
-      for(let i=0; i<scrolled.length; i++){
-        if (event.target == btns[i]){
-          scrolled[i].scrollIntoView({behavior:"smooth", block: "center"});
-        }
-      }
-    }
+  // nav.addEventListener('click', function(event) {
+  //   if(event.target && event.target.classList.contains('bt')){
+  //     for(let i=0; i<scrolled.length; i++){
+  //       if (event.target == btns[i]){
+  //         scrolled[i].scrollIntoView({behavior:"smooth", block: "center"});
+  //       }
+  //     }
+  //   }
+  
 
   });
